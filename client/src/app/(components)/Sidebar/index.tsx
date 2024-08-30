feb19a7f18073a63498fffcacd8ee9d32ca1b837
panpanwardiman <pwardiman@gmail.com>
2024-08-30 11:22:07 +0700

    navbar and sidebar

---
+++ client/src/app/(components)/Sidebar/index.tsx
@@ -0,0 +1,29 @@
+"use client"
+
+import React from 'react'
+import { Menu } from 'lucide-react'
+
+const Sidebar = () => {
+  return (
+    <div>
+        <div className='flex gap-3 justify-between md:justify-normal items-center pt-8'>
+            <div>Logo</div>
+            <h1 className='font-extrabold text-2xl'>PWARDIMAN</h1>
+        
+            <button className='md:hidden px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100' onClick={() => {}}>
+                <Menu className='w-4 h-4'/>
+            </button>
+        </div>
+
+        {/** LINKS */}
+        <div className='flex-grow mt-8'>{/** links here */}</div>
+
+        {/** FOOTER */}
+        <div>
+            <p className='text-center text-xs text-gray-500'>&copy; 2024 PWardiman</p>
+        </div>
+    </div>
+  )
+}
+
+export default Sidebar
